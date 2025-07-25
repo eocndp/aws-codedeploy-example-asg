4440cf4e0b2125d30a78f32aed4a598057719b37
Kim Jun Young <ky0422a@gmail.com>
2025-07-25 14:47:13 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World! (CodeDeploy Example)')
+    res.send('Hello World! (CodeDeploy Example) 2')
 })
 
 app.get('/health', (req, res) => {
     res.json({
         status: 'OK',
-        version: '2.0.0',
+        version: '2.0.1',
     })
 })
 
